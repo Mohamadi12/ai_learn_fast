a2916d14f37cc8dd6facabd33b981383f2d0b049
Nana Mohamadi <adilnana78@gmail.com>
2025-08-09 22:44:59 +0000

    Appeler des models

---
+++ src/app/api/toolCalling/route.ts
@@ -0,0 +1,54 @@
+import { tool, generateText } from "ai";
+import { z } from "zod";
+import { openai } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
+
+const model = openai("gpt-4.1-nano");
+// const model = anthropic("claude-instant-1")
+
+const logToConsoleTool = tool({
+  description: "Log a message to the console",
+  parameters: z.object({
+    message: z
+      .string()
+      .describe("The message to log to the console"),
+  }),
+  // @ts-ignore
+   execute: async ({ message }: { message: string }) => {
+    console.log(message);
+  },
+});
+
+// Just log to console
+const logToConsole = async (prompt: string) => {
+  await generateText({
+    model,
+    prompt,
+    system:
+      `Your only role in life is to log ` +
+      `messages to the console. ` +
+      `Use the tool provided to log the ` +
+      `prompt to the console.`,
+    tools: {
+      logToConsole: logToConsoleTool,
+    },
+  });
+};
+
+// Debug console
+const DebugConsole = async (prompt: string) => {
+  const { steps } = await generateText({
+    model,
+    prompt,
+    system:
+      `Your only role in life is to log ` +
+      `messages to the console. ` +
+      `Use the tool provided to log the ` +
+      `prompt to the console.`,
+    tools: {
+      logToConsole: logToConsoleTool,
+    },
+  });
+
+  console.dir(steps[0]?.toolCalls, { depth: null });
+};
