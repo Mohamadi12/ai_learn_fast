a8910f83927ef82740ba2442618fe8a13ebe8423
Nana Mohamadi <adilnana78@gmail.com>
2025-08-09 10:24:17 +0000

    Decouper le code et jouer avec l'IA

---
+++ src/app/api/selectModel/route.ts
@@ -0,0 +1,23 @@
+import { anthropic } from "@ai-sdk/anthropic";
+import { openai } from "@ai-sdk/openai";
+import { generateText, type LanguageModel } from "ai";
+
+export const ask = async ( prompt: string, model: LanguageModel ) => {
+  const { text } = await generateText({ model, prompt });
+
+  return text;
+};
+
+const prompt = `Tell me a story about your grandmother.`;
+
+const anthropicResult = await ask(
+  prompt,
+  anthropic("claude-3-5-haiku-latest"),
+);
+
+const openaiResult = await ask(
+  prompt,
+  openai("gpt-4.1-nano"),
+);
+
+console.log(anthropicResult);
